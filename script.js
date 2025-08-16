97d3f8be3b58a3e8980c7527e86e22a3892998eb
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-08-16 10:36:10 -0400

    Replaced style properties in JS script with classlist'

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
 "use strict";
 
 const container = document.querySelector(".container");
+const startButton = document.querySelector(".start-button");
 
-// creating a function that takes rows and columns as arguements
+// creating random color generator for the background color style
+const randomColor = function () {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+let opacity = 1;
+
+// creating a function that takes grid size as an arguement
 const grid = function (gridSize) {
-  // for loop to print divs if i is less than the amount of rows * columns
+  // Outer for loop to print divs runs x amount of times, once for each row.
   for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement("div"); // creating a new div element that will be our little etch a sketch squares
-    row.classList.add("row");
+    const row = document.createElement("div"); // creating a new div for the row
+    row.classList.add("row"); // adding row class for styling
+    // inner for loop that runs x amount of times for each row, creating x amount of squares
     for (let j = 0; j < gridSize; j++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
+      const square = document.createElement("div"); // creating a new div for the squares
+      square.classList.add("square"); // adding square class for styling/formatting
       // on mouse enter -> change the background color of the div to 'white'
       square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "white";
+        square.style.backgroundColor = randomColor();
+        if (opacity > 0) {
+          opacity -= 0.1;
+        } else {
+          opacity === 0;
+        }
+        square.style.opacity = opacity;
+        console.log(opacity);
       });
       // on mouse leave -> change the background color to nothing
       square.addEventListener("mouseleave", () => {
         square.style.backgroundColor = "";
       });
-      row.appendChild(square);
+      row.appendChild(square); // appending the squares to the parent rows
     }
-    container.appendChild(row);
+    container.appendChild(row); // appending the rows to the parent container
   }
 };
 
-grid(16);
+container.classList.add("hidden");
 
-// const button = document.createElement("button");
-// container.appendChild(button);
-// button.style.border = "2px dotted blue";
-// button.style.width = "100px";
-// button.style.height = "100px";
+// adding an event listener to our start button that expects a click
+// when clicked remove the hidden classlist from the container (show the grid)
+// and then store the answer to the prompt in 'answer'
+// pass answer through grid function as the arguement
+
+startButton.addEventListener("click", () => {
+  let answer = prompt("Enter number here: ");
+  if (answer > 100) {
+    alert("Enter a number lower than 100");
+    location.reload();
+  } else if (answer < 100) {
+    container.classList.remove("hidden");
+    grid(answer);
+  }
+});
